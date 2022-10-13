ea60ada9071ded7e33083d1170294e8f941ebcfd
akanevrc <akanezoranomukou@gmail.com>
2022-10-13 15:40:10 +0900

    fix: regexのバグを修正

--- teuchiudon-parser/src/locator/token.rs
+++ teuchiudon-parser/src/locator/token.rs
@@ -2,7 +2,7 @@ use logos::Logos;
 
 #[derive(Logos, Clone, Copy, Debug, Eq, PartialEq)]
 pub enum Token {
-    #[regex(r"~(\r\n|\r|\n)*(\r\n|\r|\n)?")]
+    #[regex(r"[^\r\n]*(\r\n|\r|\n)?")]
     Line,
 
     #[error]
