3ccffb78bb9bfc609dcfa062fdc8f9891483bf06
akanevrc <akanezoranomukou@gmail.com>
2022-10-13 13:29:28 +0900

    feat: レクサーの関数を追加

--- teuchiudon-parser/src/lexer/mod.rs
+++ teuchiudon-parser/src/lexer/mod.rs
@@ -1,3 +1,13 @@
+use self::items::LexerItemsSource;
+
 pub mod items;
 pub mod token;
 pub mod tokens;
+
+use logos::Logos;
+use token::Token;
+
+pub fn lex<'input>(input: &'input str) -> LexerItemsSource<'input, Token>{
+    let lexer = Token::lexer(input);
+    LexerItemsSource::new(lexer)
+}
