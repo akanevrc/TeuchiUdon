27efc220742cb2c7ab283951b76a66d64dedece3
akanevrc <akanezoranomukou@gmail.com>
2022-10-13 16:15:21 +0900

    Revert "test: locatorにEOF位置の仕様を追加"
    This reverts commit 6eb62c112e0ba2501c3dfcbd36617587e9581d5c.

--- teuchiudon-parser/src/test/locator.rs
+++ teuchiudon-parser/src/test/locator.rs
@@ -5,7 +5,7 @@ use crate::locator::{
 };
 
 #[test]
-fn test_normal() {
+fn test() {
     let lexer = Token::lexer("ABCDE\rabcde\n123\r\n45");
     let locations = Locations::new(lexer);
     assert_eq!(locations.lines.len(), 4);
@@ -18,22 +18,5 @@ fn test_normal() {
     assert_eq!(locations.to_line_column(14), Some((3, 3)));
     assert_eq!(locations.to_line_column(17), Some((4, 1)));
     assert_eq!(locations.to_line_column(18), Some((4, 2)));
-    assert_eq!(locations.to_line_column(19), Some((4, 3)));
-    assert_eq!(locations.to_line_column(20), None);
-}
-
-#[test]
-fn test_newline() {
-    let lexer = Token::lexer("ABCDE\rabcde\n123\r\n");
-    let locations = Locations::new(lexer);
-    assert_eq!(locations.lines.len(), 3);
-    assert_eq!(locations.to_line_column(0), Some((1, 1)));
-    assert_eq!(locations.to_line_column(2), Some((1, 3)));
-    assert_eq!(locations.to_line_column(4), Some((1, 5)));
-    assert_eq!(locations.to_line_column(6), Some((2, 1)));
-    assert_eq!(locations.to_line_column(10), Some((2, 5)));
-    assert_eq!(locations.to_line_column(12), Some((3, 1)));
-    assert_eq!(locations.to_line_column(14), Some((3, 3)));
-    assert_eq!(locations.to_line_column(17), Some((4, 1)));
-    assert_eq!(locations.to_line_column(18), None);
+    assert_eq!(locations.to_line_column(19), None);
 }
