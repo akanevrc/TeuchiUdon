e41453b54301fe0ee0c9e973ee4b0f18474e4515
akanevrc <akanezoranomukou@gmail.com>
2022-11-25 17:37:08 +0900

    feat: Scopeの未実装部分を実装

--- teuchiudon-parser/src/semantics/elements/scope.rs
+++ teuchiudon-parser/src/semantics/elements/scope.rs
@@ -2,33 +2,41 @@ use super::element::SemanticElement;
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
 pub enum Scope {
-    Block,
-    FnBlock,
-    LoopBlock,
-    For,
-    Fn,
-    Closure,
-    LetIn,
+    Block(usize),
+    Loop(usize),
+    LetIn(usize),
+    Fn(usize),
     Qual(String),
-    VarBind,
 }
 
 impl SemanticElement for Scope {
     fn description(&self) -> String {
         match self {
+            Self::Block(x) =>
+                format!("block[{}]", x),
+            Self::Loop(x) =>
+                format!("loop[{}]", x),
+            Self::LetIn(x) =>
+                format!("letin[{}]", x),
+            Self::Fn(x) =>
+                format!("fn[{}]", x),
             Self::Qual(x) =>
                 x.description(),
-            _ =>
-                panic!("Not implemented error occured"),
         }
     }
 
     fn logical_name(&self) -> String {
         match self {
+            Self::Block(x) =>
+                format!("block[{}]", x),
+            Self::Loop(x) =>
+                format!("loop[{}]", x),
+            Self::LetIn(x) =>
+                format!("letin[{}]", x),
+            Self::Fn(x) =>
+                format!("fn[{}]", x),
             Self::Qual(x) =>
                 x.logical_name(),
-            _ =>
-                panic!("Not implemented error occured"),
         }
     }
 }
